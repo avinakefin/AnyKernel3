04b082e212292e4bfdcda3d63ec75821631286d8
avinakefin <ojiappaijo@gmail.com>
2023-08-15 20:19:28 +0700

    for poco f3 yaknah

--- anykernel.sh
+++ anykernel.sh
@@ -4,25 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string= Samsoe Ginkgo Unified A11
+kernel.string=Sudah Backup Boot and DTBO
 do.devicecheck=1
-do.modules=1
-do.systemless=0
+do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
-device.name3=
-device.name4=
-device.name5=
-supported.versions=10 - 12
+device.name1=alioth
+device.name2=aliothin
+supported.versions=11 - 13
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=1;
 ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -35,11 +33,54 @@ ramdisk_compression=auto;
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+case "$ZIPFILE" in
+    *ksu*)
+    ui_print " ";
+    ui_print " ===================================  ";
+    ui_print " |            MENGGUNAKAN           | ";
+    ui_print " |             KSU MODE             | ";
+    ui_print " ===================================  ";
+    ui_print " ";
+    ui_print " ";
+    rm Image;
+    mv ksu/Image $home/Image;
+    ;;
+    *)
+    ui_print " ";
+    ui_print " ";
+    ui_print " ====> ";
+    ui_print " ====>    Default Non Root ";
+    ui_print " ====> ";
+    ui_print " ";
+    ;;
+esac
+    
 
-## AnyKernel install
+
+## AnyKernel boot install
 dump_boot;
+flash_dtbo;
 
+# begin ramdisk changes
+# end ramdisk changes
 
 write_boot;
-## end install
+## end boot install
+
+
+# shell variables
+block=vendor_boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# reset for vendor_boot patching
+reset_ak;
+
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
 
