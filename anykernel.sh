41318b90fc8501c23fecb5742eaa58944fb7bab0
avinakefin <ojiappaijo@gmail.com>
2023-08-15 20:10:32 +0700

    for poco f5

--- anykernel.sh
+++ anykernel.sh
@@ -4,42 +4,50 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string= Samsoe Ginkgo Unified A11
+kernel.string=Sudah backup BOOT dan DTBO ?
 do.devicecheck=1
-do.modules=1
-do.systemless=0
+do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
+device.name1=marble
+device.name2=marblein
 device.name3=
 device.name4=
 device.name5=
-supported.versions=10 - 12
-supported.patchlevels=
+supported.versions=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+slot=$(find_slot);
+block=/dev/block/bootdevice/by-name/boot$slot;
+is_slot_device=1;
 ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-
 ## AnyKernel install
 dump_boot;
+flash_dtbo;
 
+## Selinux Permissive
+#patch_cmdline androidboot.selinux androidboot.selinux=permissive
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+# end ramdisk changes
 
 write_boot;
 ## end install
-
